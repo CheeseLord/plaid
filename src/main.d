de62083807d86ee6f2e3772d64ace610b82d57b2
Greg Kronmiller <greg_kronmiller@hmc.edu>
2015-09-19 15:42:40 -0700

    Add FIXME to code for dummy issue.

--- src/main.d
+++ src/main.d
@@ -4,7 +4,10 @@ import mylib;
 
 void main()
 {
+    // FIXME [#1]: Remove this exclamation mark, once we can figure out how to
+    // use a text editor.
     writefln("Hello, world!");
+
     writefln("One more than 5 is: %s", successor(5));
     writefln("Twice 5 is: %s", twice(5));
 }
