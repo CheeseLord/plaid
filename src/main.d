f7f7214e0b6cfcec35bde2aa4c36eff27810f51e
Greg Kronmiller <greg_kronmiller@hmc.edu>
2015-09-19 15:32:02 -0700

    Figured out how to build documentation.
    Put instructions for that (and other dub commands) in new file
    DUB_INSTRUCTIONS.txt. Added a dummy library with a couple functions,
    just to test that documentation builds.

--- src/main.d
+++ src/main.d
@@ -1,6 +1,10 @@
 import std.stdio;
 
+import mylib;
+
 void main()
 {
     writefln("Hello, world!");
+    writefln("One more than 5 is: %s", successor(5));
+    writefln("Twice 5 is: %s", twice(5));
 }
---
+++ src/mylib.d
@@ -0,0 +1,15 @@
+/**
+ * Add one to the input.
+ */
+int successor(int x) pure
+{
+    return x + 1;
+}
+
+/**
+ * Double the input.
+ */
+int twice(int x) pure
+{
+    return x + x;
+}
