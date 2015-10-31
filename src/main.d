6bf97de2de04fae19f02d23d48a6f6ae722601a3
Greg Kronmiller <kronmillerg@gmail.com>
2015-10-31 14:39:21 -0700

    Remove unneeded starter code.

--- src/main.d
+++ src/main.d
@@ -1,20 +1,11 @@
 import std.stdio;
 
-import mylib;
-
 // import GTKTest;
 import SDLExample;
 
 void main()
 {
-    // FIXME [#1]: Remove this exclamation mark, once we can figure out how to
-    // use a text editor.
     writefln("Hello, world!");
-
-    writefln("One more than 5 is: %s", successor(5));
-    writefln("Twice 5 is: %s", twice(5));
-
-    // TODD [#2]: Say goodbye to the world, for symmetry.
 
     // testWindow();
     testSDL();
