e46cdb25bce2f9e14171f5559e66c9f833b37fc5
Kyle Fox <fox_kyle@yahoo.com>
2015-11-07 16:36:17 -0500

    Factored out event handling.

--- src/main.d
+++ src/main.d
@@ -4,6 +4,7 @@ import core.thread;
 
 import derelict.sdl2.sdl;
 
+import eventHandler;
 import globals;
 // import GTKTest;
 
@@ -69,35 +70,6 @@ void runGame()
                                (MonoTime.currTime - currStartTime);
         if (!timeToSleep.isNegative) {
             Thread.sleep(timeToSleep);
-        }
-    }
-}
-
-void handleEvents()
-{
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-        // Handle the next event here. To get the type of event (keypress,
-        // mouse motion, close window, etc.), look at event.type.
-        // For a list of possible event types, see
-        //     https://wiki.libsdl.org/SDL_EventType
-        //
-        // If the type is some sort of keypress, you'll want to look at the
-        // actual key that was pressed, stored in event.key.keysym. See
-        //     https://wiki.libsdl.org/SDL_Keysym
-        // for information on interpreting these. That page has links to the
-        // lists o key codes and scan codes.
-        if (event.type == SDL_QUIT) {
-            shouldQuit = true;
-        }
-        else if (event.type == SDL_KEYDOWN) {
-            switch (event.key.keysym.sym) {
-                case SDLK_UP:    xVel =    0; yVel = -100; break;
-                case SDLK_RIGHT: xVel =  100; yVel =    0; break;
-                case SDLK_DOWN:  xVel =    0; yVel =  100; break;
-                case SDLK_LEFT:  xVel = -100; yVel =    0; break;
-                default: // Ignore other keys.
-            }
         }
     }
 }
