7004e334b3fd4b301576fd1cec2936ffacca26c0
Kyle Fox <fox_kyle@yahoo.com>
2015-12-12 18:08:28 -0500

    Cleaned up setup and cleanup.

--- src/setup_cleanup.d
+++ src/setup_cleanup.d
@@ -10,79 +10,20 @@ import globals;
 import platform_functions;
 import yaml_parser;
 
-// Initialize everything. On success, return true. On failure, report the error
-// and return false.
+// Initialize everything. 
 bool setup()
 {
-    // Load the SDL shared library.
-    // The exception handling code here is adapted from
-    //     https://github.com/DerelictOrg/DerelictUtil/wiki/DerelictUtil-for-Users:
-    try {
-        DerelictSDL2.load();
-    }
-    catch (SymbolLoadException e) {
-        stderr.writefln("Error: Derelict SDL2 failed to load a symbol: %s",
-                        e.msg);
-        return false;
-    }
-    catch (SharedLibLoadException e) {
-        stderr.writefln("Error: Failed to load SDL2 library: %s", e.msg);
-        for (Throwable curr = e.next; curr !is null; curr = curr.next) {
-            stderr.writefln("    Further info: %s", curr.msg);
-        }
-        return false;
-    }
-    catch (Exception e) {
-        stderr.writefln("Error: Failed to load Derelict SDL2: %s", e.msg);
-        return false;
-    }
+    bool success = true;
 
-    // Initialize SDL.
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("Error: Failed to initialize SDL: %s\n",
-               SDL_GetError());
-        return false;
-    }
-    initialize_magic_numbers();
+    initializeMagicNumbers();
+    success &= setupLibraries();
+    success &= setupWindow();
 
-    // Create a window.
-    // TODO [#3]: Magic numbers bad.
-    window = SDL_CreateWindow("Plaid",
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-        640, 480, SDL_WINDOW_OPENGL);
-
-    if (window is null) {
-        writefln("Error: Failed to create window for plaid.");
-        return false;
-    }
-
-    return true;
-}
-
-// Clean up everything that was initialized. On success, return true. On
-// failure, report the failure and return false. Not that there's much the
-// caller can do about a failure to cleanup....
-bool cleanup()
-{
-    cleanup_objects();
-    debug writefln("Exiting.");
-
-    if (window !is null) {
-        SDL_DestroyWindow(window);
-    }
-
-    // From http://wiki.libsdl.org/SDL_Quit:
-    //     "You should call this function even if you have already shutdown
-    //      each initialized subsystem with SDL_QuitSubSystem(). It is safe to
-    //      call this function even in the case of errors in initialization."
-    // Therefore, don't worry about checking if the SDL_Init succeeded.
-    SDL_Quit();
-
-    return true;
+    return success;
 }
 
 // TODO [#3]: Magic numbers bad.
-void initialize_magic_numbers()
+void initializeMagicNumbers()
 {
     Node configRoot = Loader("config/magic.yaml").load();
     // // This is so not Windows compatible it's not even funny.
@@ -149,7 +90,120 @@ void initialize_magic_numbers()
     shouldQuit = false;
 }
 
-void cleanup_objects(){
+// Set up everything associated with each library we use.
+bool setupLibraries()
+{
+    bool success = true;
+
+    success &= setupSDL();
+
+    return success;
+}
+
+// Set up everything associated with SDL.
+bool setupSDL()
+{
+    // Load the SDL shared library.
+    // The exception handling code here is adapted from
+    //     https://github.com/DerelictOrg/DerelictUtil/wiki/DerelictUtil-for-Users:
+    try {
+        DerelictSDL2.load();
+    }
+    catch (SymbolLoadException e) {
+        stderr.writefln("Error: Derelict SDL2 failed to load a symbol: %s",
+                        e.msg);
+        return false;
+    }
+    catch (SharedLibLoadException e) {
+        stderr.writefln("Error: Failed to load SDL2 library: %s", e.msg);
+        for (Throwable curr = e.next; curr !is null; curr = curr.next) {
+            stderr.writefln("    Further info: %s", curr.msg);
+        }
+        return false;
+    }
+    catch (Exception e) {
+        stderr.writefln("Error: Failed to load Derelict SDL2: %s", e.msg);
+        return false;
+    }
+
+    // Initialize SDL.
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Error: Failed to initialize SDL: %s\n",
+               SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+// Set up the game window.
+bool setupWindow()
+{
+    // Create a window.
+    // TODO [#3]: Magic numbers bad.
+    window = SDL_CreateWindow("Plaid",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        640, 480, SDL_WINDOW_OPENGL);
+
+    if (window is null) {
+        writefln("Error: Failed to create window for plaid.");
+        return false;
+    }
+
+    return true;
+}
+
+// Clean up everything that was initialized.
+bool cleanup()
+{
+    debug writefln("Exiting.");
+
+    bool success = true;
+
+    success &= cleanupObjects();
+    success &= cleanupWindow();
+    success &= cleanupLibraries();
+
+    return true;
+}
+
+// Clean up game objects.
+bool cleanupObjects()
+{
     // Nothing to do right now
+    return true;
+}
+
+// Clean up the game window.
+bool cleanupWindow()
+{
+    if (window !is null) {
+        SDL_DestroyWindow(window);
+    }
+
+    return true;
+}
+
+// Clean up everything associated with each library we're using.
+bool cleanupLibraries()
+{
+    bool success = true;
+
+    success &= cleanupSDL();
+
+    return success;
+}
+
+// Clean up everything associated with SDL.
+bool cleanupSDL()
+{
+    // From http://wiki.libsdl.org/SDL_Quit:
+    //     "You should call this function even if you have already shutdown
+    //      each initialized subsystem with SDL_QuitSubSystem(). It is safe to
+    //      call this function even in the case of errors in initialization."
+    // Therefore, don't worry about checking if the SDL_Init succeeded.
+    SDL_Quit();
+
+    return true;
 }
 
