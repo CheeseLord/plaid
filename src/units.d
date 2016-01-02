80f15ab9b781c126dabd9505e7cc453209ebfcbe
Greg Kronmiller <kronmillerg@gmail.com>
2016-01-02 15:20:53 -0800

    Create units.d with typedefs for length and time types.

---
+++ src/units.d
@@ -0,0 +1,10 @@
+module units;
+
+// Type used for lengths in the world. Given in "arbitrary units of length"
+// (aul). 2^8 aul = player width.
+alias world_length = int;
+
+// Type used for times in the world. Given in "arbitrary units of time" (aut).
+// 2^16 aut = 1 second.
+alias world_time   = int;
+
