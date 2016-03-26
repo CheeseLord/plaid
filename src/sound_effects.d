ce8c16ad8e92c73cc1ba2db71b16e4e1b0bd8231
Greg Kronmiller <kronmillerg@gmail.com>
2016-03-26 15:11:50 -0700

    Play "jump" and "land" sound effects.

--- src/sound_effects.d
+++ src/sound_effects.d
@@ -4,6 +4,7 @@ private import std.stdio;
 
 private import globals;
 private import observer;
+private import sound;
 
 
 public void registerObservers()
@@ -14,14 +15,18 @@ public void registerObservers()
 
 private void makeSoundEffect(NotifyType eventInfo)
 {
-    // TODO [#37]: Actually make sounds.
     if (eventInfo == NotifyType.PLAYER_JUMP) {
         debug writefln("Jump.");
+        Sound.playSe("jump.wav");
+        Sound.playMarkedSes();
     }
     else if (eventInfo == NotifyType.PLAYER_LAND) {
         debug writefln("Land.");
+        Sound.playSe("land.wav");
+        Sound.playMarkedSes();
     }
     else if (eventInfo == NotifyType.PLAYER_BOUNCE) {
+        // TODO [#37]: Actually play a sound.
         debug writefln("Bounce.");
     }
 }
